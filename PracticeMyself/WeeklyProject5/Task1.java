c72d3366e0b22d192461b3f932ff9e12d1d1497b
ugurucar45 <ugurucar45@gmail.com>
2021-12-18 12:54:03 -0600

    updated

--- PracticeMyself/WeeklyProject5/Task1.java
+++ PracticeMyself/WeeklyProject5/Task1.java
@@ -15,19 +15,18 @@ public class Task1 {
         System.out.println("Please enter a string");
         String str = sc.nextLine();
         String rts = "";
-        str=str.trim();
-//             char a=' ';
-//        for (int b=0; a == str.charAt(b); b++) {
-//            if (str.substring(0, 1).equals(" ")) {
-//                str = str.substring(1);
-//            }
-//        }
-//      //  for ( int c = str.length(); a == str.crrrrrrrrrrrrrrrrrharAt(c); c--) {
-//            if (str.substring(str.length() - 1).equals(" ")) {
-//                str = str.substring(0, str.length() - 1);
-//                //c--;
-//        //    }
-//        }
+//        str=str.trim();                                       //or it is really easy another way
+        for (int b = 0; ' ' == str.charAt(b); b++) {            //like trim method
+            if (str.substring(0, 1).equals(" ")) {
+                str = str.substring(1);
+                b--;
+            }
+        }                                                       //easy way trim
+        for (int c = str.length() - 1; ' ' == str.charAt(c); c--) {
+            if (str.substring(str.length() - 1).equals(" ")) {
+                str = str.substring(0, str.length() - 1);
+            }
+        }
         for (int i = str.length() - 1; i >= 0; i--) {
             rts = rts + str.substring(i, i + 1);
         }
