036cfe103f130423abab5fee811e2db848ce6070
ugurucar45 <ugurucar45@gmail.com>
2021-12-21 20:04:57 -0600

    updated

---
+++ Object/Animal.java
@@ -0,0 +1,18 @@
+package Object;
+
+public class Animal {
+
+    String name;      // declared a variable and named it "animal"
+    int age;          // global/instance variable
+    String color;     // global/instance variable.  SO FAR NO OBJECT
+    String breed="Boxer";   //it is default value
+
+
+
+
+
+
+    public static void main(String[] args){
+        String lastName;
+    }
+}
