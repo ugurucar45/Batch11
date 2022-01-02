f4269d3dd05c7c157a8cabfabfe13bcfc203fcfd
ugurucar45 <ugurucar45@gmail.com>
2022-01-02 13:46:56 -0600

    Java Practice

--- wrapperClass/Practice1.java
+++ wrapperClass/Practice1.java
@@ -1,9 +1,16 @@
 package wrapperClass;
 
+import object.Car;
+import object.Student;
+
 public class Practice1 {
     public static void main(String[] args) {
         byte b=2;
         Byte bt=new Byte(b);
-        System.out.println(bt);
+        System.out.println(bt);//2
+        Student st=new Student();
+        System.out.println(st);//hash code
+        Car car5=new Car("Honda");
+        System.out.println(car5);//My car's brand is Honda
     }
 }
