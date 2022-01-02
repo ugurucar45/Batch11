8da995f0a07f945117408848a667c61ff0884733
ugurucar45 <ugurucar45@gmail.com>
2022-01-02 13:43:01 -0600

    Java Practice

--- wrapperClass/Practice1.java
+++ wrapperClass/Practice1.java
@@ -2,6 +2,8 @@ package wrapperClass;
 
 public class Practice1 {
     public static void main(String[] args) {
-
+        byte b=2;
+        Byte bt=new Byte(b);
+        System.out.println(bt);
     }
 }
