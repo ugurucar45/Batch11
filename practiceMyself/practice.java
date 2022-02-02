d0f94b2cca766643a3f1978900a41e2573d9446c
ugurucar45 <ugurucar45@gmail.com>
2022-02-01 21:32:17 -0600

    Java Practice

---
+++ practiceMyself/practice.java
@@ -0,0 +1,21 @@
+package practiceMyself;
+
+public class practice {
+    public static void main(String[] args) {
+
+        byte        ss      =       1       ;
+        short       sh      =       2       ;
+        int         in      =       3       ;
+        long        lon     =       3       ;
+
+        float       fl      =       1.5f    ;
+        double      doub    =       22.5    ;
+
+        char        ch      =       '9'     ;
+        boolean     boo     =       true    ;
+
+        String      sss     =       "ugur likes apple but dont like ice 1121125784594314 j true false 1.5 2,5,35432dsaz,./,.mnmasfhuaisry2878877**()_&)3"    ;
+
+        System.out.println(sss);
+    }
+}
